c1fdc28eff2a944afd1d738c81cfaca36470d9b0
panduramadhan2 <panduramadhan2@gmail.com>
2024-06-11 10:33:53 +0700

    product service checkpoint & boiler plate

---
+++ product-service/product-service/src/main/java/com/programmingtechie/product_service/service/ProductService.java
@@ -0,0 +1,11 @@
+package com.programmingtechie.product_service.service;
+
+import com.programmingtechie.product_service.dto.ProductRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+    public void createProduct(ProductRequest productRequest){
+
+    }
+}
